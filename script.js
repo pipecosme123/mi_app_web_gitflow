86d5f66da11b011b34e651130f86b18b4150f407
La Tecnología Avanza <cramirezchristian77@gmail.com>
2024-07-23 02:50:25 -0500

    Corregir error crítico

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
 });
 
-//Este es un metodo, suscríbete a la tecnologia avanza
+//Este es un metodo
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var name = document.getElementById('name').value;
