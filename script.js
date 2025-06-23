2b326aa867a52dc23ab0a5a809f0b3c6f9ddf184
Daniel Cosme <pipecosme123@gmail.com>
2025-06-23 08:03:32 -0500

    Formulario de contacto

--- script.js
+++ script.js
@@ -7,5 +7,6 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     event.preventDefault();
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
-    document.getElementById('formOutput').textContent = 'Nombre: ' + name + ', Email: ' + email;
+    var tel = document.getElementById('tel').value;
+    document.getElementById('formOutput').textContent = 'Nombre: ' + name + ', Email: ' + email + ', Teléfono: ' + tel;
 });
