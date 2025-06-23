5f131252ef6250806ec0639e966be3ad6502624a
Daniel Cosme <pipecosme123@gmail.com>
2025-06-23 08:17:15 -0500

    Preparando todo para producción

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('btnClick').addEventListener('click', function() {
     document.getElementById('output').textContent = '¡Botón clicado!';
 });
 
-//Este es un metodo
+//Este es un metodo para obtener el valor de un input y mostrarlo en un div
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
     var name = document.getElementById('name').value;
